9e75c6351e015566f333a9d40221f085c91c4dcc
JohnHartnett5123 <jhartnett2019@gmail.com>
2020-09-10 14:39:21 -0500

    9/10

---
+++ src/App.js
@@ -0,0 +1,142 @@
+import React from 'react'
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
+import ReactDOMServer from 'react-dom/server';
+
+// const [typeArg, KeyboardEventInit]
+
+// const event = new KeyboardEvent(typeArg, KeyboardEventInit);
+
+
+
+const Dictaphone = (props) => {
+
+  constructor(props); {
+    super(props);
+    console.log(this.props);
+  }
+
+  const { transcript, resetTranscript } = useSpeechRecognition()
+
+
+
+  if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
+    return null
+  }
+
+  // if (!SpeechRecognition.startListening()){
+  //   console.log("stopped listening")
+  // }
+
+  return (
+    <div>
+      <button onClick={SpeechRecognition.startListening({ continuous: true })}>Start</button>
+      <button onClick={SpeechRecognition.stopListening}>Stop</button>
+      <button onClick={resetTranscript}>Reset</button>
+      <p>{transcript}</p>
+    </div>
+  )
+}
+export default Dictaphone
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { useState, useEffect } from 'react';
+
+// import './App.css';
+
+
+// const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+// const recognition = new SpeechRecognition()
+
+// recognition.start()
+
+
+// function App() {
+//   const [count, setCount] = useState(0)
+//   let [words, setWords] = useState()
+
+
+
+//   const voiceCommands = () => {
+//     recognition.onstart = () => {
+//       console.log("big brother is listening")
+//     }
+
+//     recognition.onresult = (e) => {
+//       let current = e.resultIndex;
+
+//       let transcript = e.results[current][0].transcript
+
+//       console.log(transcript)
+
+
+//       let mobileRepeatBug = (current === 1 && transcript === e.results[0][0].transcript)
+
+//       if (!mobileRepeatBug){
+//         setWords(words = transcript)
+//         if (transcript === 'next' || transcript === " next"){
+//           setCount(count+1)
+//         }
+//         if (transcript === 'back' || transcript === " back"){
+//           setCount(count-1)
+//         }
+//       }
+
+//       setTimeout(() => {
+//         recognition.start()
+//       },50)
+
+//     }
+
+//     recognition.onspeechend = () => {
+//       recognition.stop()
+//       console.log("thank god it stopped speaking")
+//     }
+
+//   }
+
+
+//   useEffect(() => {
+//     voiceCommands()
+//   })
+
+//   return (
+//     <div className="App">
+//       <h1>counter: {count}</h1>
+//       <button onClick={() => setCount(count + 1)}>+</button>
+//       <button onClick={() => setCount(count - 1)}>-</button>
+//     <p>{words}</p>
+//     </div>
+//   );
+// }
+
+// export default App;
